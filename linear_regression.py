9dbd51a1e34c350b95f7c4572438817adee48171
meetofleaf <meetofleaf@gmail.com>
2024-03-07 23:05:04 -0500

    added additional comments

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,4 @@
+# Import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,36 +6,37 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+# Import Dataset
+dataset = pd.read_csv('<file_path>')    # Replace <file_path> with the file path of the dataset within the quotes.
 
-dataset = pd.read_csv('<file_path>')
 
-
-# Calculating study time per course for each student
+# Calculating study time per course for each student. This can also be used as independent variable.
 #dataset['study_time_per_course'] = dataset['time_study'] / dataset['number_courses']
 
-# COLUMNS: number_courses, time_study, Marks, study_time_per_course
+# Columns in order after adding the new variable/column: number_courses, time_study, Marks, study_time_per_course
 
-# Extracting Independent (X) and dependent set (y)
+# Extracting independent (X) and dependent (y) columns/fields/variables. You can experiment with different independent variables.
 X = dataset.iloc[:,1:2].values   # time_study
 y = dataset.iloc[:,2].values    # Marks
+# Reference for iloc: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iloc.html
 
 
-# Splitting the dataset into training and Test set
+# Splitting the dataset into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-print(X)
-print(y)
 
 
-# Train the Linear Regression model on the training set
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+# Training the Linear Regression model (named regressor here) on the training set
+regressor = LinearRegression()      # Define regressor as linear regression model
+regressor.fit(X_train, y_train)     # Train the model on training data of both independent and dependent data OR
+                                    # you could simply say, teaching the model using the training data OR
+                                    # fit the model on the training data, hence function name 'fit'
 
 
-# Predicting the dependent values using test set results
+# Predicting the dependent values using the test set of independent data
 y_pred = regressor.predict(X_test)
+# Simply, after learning from training data, the model tries to predict the dependent data (y_pred) from independent data (X_test)
 
-
-### You're done with the Machine Learning part, now let's visualize our result
+### You're done with the Machine Learning part, now let's visualize our results
 
 # Visualizing Training set results
 plt.scatter(X_train, y_train, color = 'red')
@@ -53,8 +55,9 @@ plt.ylabel('Marks')
 plt.show()
 
 
-# Function to predict marks based on input study time per course using previously trained regressor
-def predictor(study_time_per_course):
-    return regressor.predict([[study_time_per_course]])
+# Function to predict marks based on input using previously trained model (regressor)
+def predictor(input_value):
+    return regressor.predict([[input_value]])
 
-print(predictor(3))
+print(predictor(3)) # Based on the independent data that you've set, change the input value from 3 (default) to any value you like.
+                    # For eg. if you chose to use study time (time_study) as independent variable, then you could enter any reasonable time in hours (1.5, 5, 7 etc.)
